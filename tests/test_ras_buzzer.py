acb1604f1a3dd2f3e4b2e843f829520347437fa7
hveficent <hector.villarreal@eficent.com>
2018-09-27 16:23:47 +0200

    test_ras_buzzer fixed

--- tests/test_ras_buzzer.py
+++ tests/test_ras_buzzer.py
@@ -1,9 +1,15 @@
-from ..lib import PasBuz
+from lib import PasBuz
 import time
-Pin = 13
-P=PasBuz.PasBuz(Pin)
-P.CheckIn()
-time.sleep(1)
-P.CheckOut()
-time.sleep(1)
-P.BuzError()
+
+if __name__ == '__main__':
+    # Pin to feed the Signal to the Buzzer - Signal Pin
+    PinSignalBuzzer = 13
+    # Pin for the feeding Voltage for the Buzzer - Power Pin
+    PinPowerBuzzer = 12
+    # Creating one Instance for our Passive Buzzer
+    PBuzzer = PasBuz.PasBuz(PinSignalBuzzer, PinPowerBuzzer)
+    PBuzzer.CheckIn()
+    time.sleep(1)
+    PBuzzer.CheckOut()
+    time.sleep(1)
+    PBuzzer.BuzError()
